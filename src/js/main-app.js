1bfb384bd6084556e253da819ee15df50a18155b
morganmcfalljohnsen <morganmcfalljohnsen2015@u.northwestern.edu>
2019-02-13 17:20:07 -0600

    Commit message

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -13,3 +13,23 @@ window.$('.bt-icon--twitter').click((e) => {
   const status = encodeURIComponent(`${window.tweetText} ${uri}`);
   window.open(`https://twitter.com/intent/tweet?text=${status}`);
 });
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+     if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
