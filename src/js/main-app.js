761b22ec6ee6e34460a88aec343caeb7e17fdb85
morganmcfalljohnsen <morganmcfalljohnsen2015@u.northwestern.edu>
2019-02-19 23:21:26 -0600

    collapsibles not working

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -33,25 +33,42 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+
 var coll = document.getElementsByClassName("collapsible");
-$(".collapsible").click(function() {
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
   // get second class name (candidate's id) and save as variable
   // display the corresponding candidate-info div
   // hide the rest of the candidate-info divs
-  if ()
-})
 
-$(".tablinks").click(function() {
-  var i, tabcontent, tablinks;
-  var cityName = $(this).attr("data-id");
-  tabcontent = document.getElementsByClassName("tabcontent");
-  for (i = 0; i < tabcontent.length; i++) {
-    tabcontent[i].style.display = "none";
-  }
-  tablinks = document.getElementsByClassName("tablinks");
-  for (i = 0; i < tablinks.length; i++) {
-    tablinks[i].className = tablinks[i].className.replace(" active", "");
-  }
-  document.getElementById(cityName).style.display = "block";
-  $(this).className += " active";
+
+
+  $(".tablinks").click(function() {
+    var i, tabcontent, tablinks;
+    var cityName = $(this).attr("data-id");
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(cityName).style.display = "block";
+    $(this).className += " active";
 });
+
+
+
