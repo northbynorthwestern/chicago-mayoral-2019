6311d1c83ecc5de8893aabea433195257146381c
Audrey DeBruine <audrey.debruine@gmail.com>
2019-02-20 08:59:44 -0600

    tabs functioning

--- src/js/main-app.js
+++ src/js/main-app.js
@@ -17,36 +17,42 @@ window.$('.bt-icon--twitter').click((e) => {
 
 
 var coll = document.getElementsByClassName("collapsible");
+
 var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
+
+    var can = $(this).attr("data-id");
+    var currcan = $("."+can);
+    console.log(can + " " + currcan);
+
+    if ($(currcan).hasClass("active")) {
+      $(currcan).removeClass("active");
     } else {
-      content.style.display = "block";
+      $(currcan).addClass("active");
     }
-     if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
+    //var content = this.nextElementSibling;
+    // if (content.style.display === "block") {
+    //   content.style.display = "none";
+    // } else {
+    //   content.style.display = "block";
+    // }
+    //  if (content.style.maxHeight){
+    //   content.style.maxHeight = null;
+    // } else {
+    //   content.style.maxHeight = content.scrollHeight + "px";
+    // }
   });
 }
 
 
-
-  // get second class name (candidate's id) and save as variable
-  // display the corresponding candidate-info div
-  // hide the rest of the candidate-info divs
-
-
-
   $(".tablinks").click(function() {
     var i, tabcontent, tablinks;
-    var cityName = $(this).attr("data-id");
+    var canName = $(this).attr("data-can");
+    var tab = $(this).attr("data-id") + " " + $(this).attr("data-can");
+    console.log(tab);
     tabcontent = document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
@@ -55,9 +61,6 @@ for (i = 0; i < coll.length; i++) {
     for (i = 0; i < tablinks.length; i++) {
       tablinks[i].className = tablinks[i].className.replace(" active", "");
     }
-    document.getElementById(cityName).style.display = "block";
+    document.getElementById(tab).style.display = "block";
     $(this).className += " active";
 });
-
-
-
